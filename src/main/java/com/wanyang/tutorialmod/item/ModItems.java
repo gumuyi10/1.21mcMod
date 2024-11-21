c324111242099b5fff280d9b61d709b06d1bf2b9
gumuyi <3270187603@qq.com>
2024-11-21 13:41:26 +0800

    Commit Message

--- src/main/java/com/wanyang/tutorialmod/item/ModItems.java
+++ src/main/java/com/wanyang/tutorialmod/item/ModItems.java
@@ -8,11 +8,6 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item ICE_ETHER = registerItems("ice_ether",new Item(new FabricItem.Settings()));
-//    物品注册方法
-    private static Item registerItems(String name, Item item) {
-        return Registry.register(Registries.ITEM,new Identifier(TutorialMod.MOD_ID,name),item);
-    }
 //    初始化Mod
     public static void registerModItems() {}
 }
